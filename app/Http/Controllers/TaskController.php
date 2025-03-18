273ae2e5e395f9723dd1f19173340c832b4d7043
arracheille <arachelrabbani@gmail.com>
2025-03-18 23:15:27 +0700

    task mvc

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        $tasks = $request->validate([
+            'task' => 'required',
+            'priority' => 'required',
+            'due_date' => 'required',
+            'status' => 'required',
+        ]);
+
+        $tasks['task'] = $request->task;
+        $tasks['priority'] = $request->priority;
+        $tasks['due_date'] = $request->due_date;
+        $tasks['status'] = $request->status;
+
+        Task::create($tasks);
+
+        return redirect()->route('task.index');
+    }
+
+    public function edit(Task $task)
+    {
+        return view('tasks.index', compact('task'));
+    }
+
+    public function update(Task $task)
+    {
+        $task->update(['status' => 1]);
+
+        return redirect()->route('task.index')->with('success', 'Task berhasil diselesaikan');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('task.index')->with('success', 'Task berhasil dihapus!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $tasks = Task::orderBy('status', 'asc')
+                 ->orderBy('priority', 'desc')
+                 ->orderBy('due_date', 'asc')
+                 ->get();
+    return view('welcome', compact('tasks'));
+})->name('task.index');
+
+Route::post('/post-task', [TaskController::class, 'store'])->name('task.store');
+Route::get('/edit-task/{task}', [TaskController::class, 'edit'])->name('task.edit');
+Route::put('/edit-task/{task}', [TaskController::class, 'update'])->name('task.update');
+Route::delete('/delete-task/{task}', [TaskController::class, 'destroy'])->name('task.delete');
